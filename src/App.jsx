f6e9b9a5815a34178f8375be0c197c0e4492fe91
ankitkothe2119 <135228960+ankitkothe2119@users.noreply.github.com>
2023-09-08 00:43:29 +0530

    landing page using react
    for internship

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'; 
+import Navigation from './components/Navigation';
+import ProductSecsion from './components/ProductSecsion';
+ 
+ 
+ const App = () => {
+    return(
+        <div>
+            <Navigation/>
+            <ProductSecsion/>
+        </div>
+    );
+ };
+
+ export default App;
---
+++ src/components/ProductSecsion.jsx
@@ -0,0 +1,25 @@
+const ProductSecsion = () => {
+    return (
+        <main className="ProductSecsion container">
+            <div className="content">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+                <div className="btn">
+                    <button className="ShopNow">Shop Now</button>
+                    <button className="Category">Category</button>
+                </div>
+                <div className="availableOn">
+                    <p>Also Available On</p>
+                    <div className="platforms">
+                        <img src="/images/amazon.png" alt="amazon" />
+                        <img src="./public/images/pfliplkart.png" alt="fliplkart" />
+                    </div>
+                </div>
+            </div>
+            <div className="productImage">
+                <img src="/images/shoe_image.png" alt="shoe_image" />
+            </div>
+        </main>
+    );
+};
+export default ProductSecsion;
